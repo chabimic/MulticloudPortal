d645c101dc298beaea2b198bb94058bd1e224d28
Michal Chabiera <michal.advnet@gmail.com>
2019-05-16 12:32:52 +0200

    Component responsible for adding ne tenant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {MatButtonModule, MatMenuModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatMenuModule, MatCardModule, MatToolbarModule, MatTabsModule, MatStepperModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HorizontalMenuComponent } from './horizontal-menu/horizontal-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddTenantComponent } from './add-tenant/add-tenant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HorizontalMenuComponent
+    HorizontalMenuComponent,
+    AddTenantComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,7 +22,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
